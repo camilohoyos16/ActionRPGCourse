c715a76ba13b76b393aaa5b6dc8d48e4693cc8de
camilohoyos16 <acamilohoyos@hotmail.com>
2018-12-17 23:24:15 -0500

    First commit. Begining course

---
+++ Assets/Scripts/InputPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPlayer : MonoBehaviour {
+
+    [HideInInspector] public float horizontalAxis;
+    [HideInInspector] public float verticalAxis;
+    [HideInInspector] public bool attack;
+    [HideInInspector] public bool ability1;
+    [HideInInspector] public bool ability2;
+    [HideInInspector] public bool inventory;
+    [HideInInspector] public bool interact;
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        attack = Input.GetButtonDown("Attack");
+        ability1 = Input.GetButtonDown("Ability1");
+        ability2 = Input.GetButtonDown("Ability2");
+        inventory = Input.GetButtonDown("Inventory");
+        interact = Input.GetButtonDown("Interact");
+
+        //Axis
+        horizontalAxis = Input.GetAxis("Horizontal");
+        verticalAxis = Input.GetAxis("Vertical");
+        Debug.LogFormat("Horizontal: {0} - Vertical: {1}", horizontalAxis, verticalAxis);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float velocity;
+
+    private float horizontal;
+    private float vertical;
+    private InputPlayer playerInputInstance;
+    private Transform m_Transform;
+    private Rigidbody2D m_RigidBody2D;
+
+	// Use this for initialization
+	void Start () {
+        playerInputInstance = GetComponent<InputPlayer>();
+        m_Transform = GetComponent<Transform>();
+        m_RigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        horizontal = playerInputInstance.horizontalAxis;
+        vertical = playerInputInstance.verticalAxis;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+
+        Vector2 velocityVector = new Vector2(horizontal, vertical) * velocity;
+        //m_RigidBody2D.AddForce(force);
+        m_RigidBody2D.velocity = velocityVector;
+    }
+}
